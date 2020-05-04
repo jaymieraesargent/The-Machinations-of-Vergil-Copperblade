291b03332eabb0a739944242413707c85f88c75d
ramkillnani <54158723+ramkillnani@users.noreply.github.com>
2020-05-04 16:30:08 +1000

    Rigidbody movement
    qqq

---
+++ Assets/Scripts/CharactetController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactetController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpSpeed;
+    private float verticalDirection;
+    private float horizontalDirection;
+
+
+
+    void Update()
+    {
+
+        verticalDirection = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        horizontalDirection = Input.GetAxis("Horizontal") * moveSpeed / 3 * Time.deltaTime;
+        transform.Translate(horizontalDirection, 0, verticalDirection);
+
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            var rigid = this.gameObject.GetComponent<Rigidbody>();
+            if (rigid != null)
+            {
+                rigid.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
+            }
+        }
+    }
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
