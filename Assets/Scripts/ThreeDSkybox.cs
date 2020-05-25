c85edcd9f4ec4e005126ee7a771c1e778c16e115
GlitcherOG <lachlankalf@gmail.com>
2020-05-25 19:00:42 +1000

    3D Skybox

---
+++ Assets/Scripts/ThreeDSkybox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeDSkybox : MonoBehaviour
+{
+    public GameObject Skyboxcamera;
+    public GameObject Playercamera;
+    public float Scale;
+    private void Update()
+    {
+        if (Playercamera != null)
+        {
+            Skyboxcamera.transform.localPosition = new Vector3(Playercamera.transform.position.x / Scale, Playercamera.transform.position.y / Scale, Playercamera.transform.position.z / Scale);
+            Skyboxcamera.transform.rotation = Playercamera.transform.rotation;
+        }
+        else if(Camera.main==true)
+        {
+            Playercamera = Camera.main.gameObject;
+        }
+    }
+}
