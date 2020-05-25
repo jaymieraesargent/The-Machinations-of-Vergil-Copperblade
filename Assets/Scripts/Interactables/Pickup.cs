2e8169e98aff03d4a5b89dc42af98fa4933e9926
Yuzu-x <54381207+Yuzu-x@users.noreply.github.com>
2020-05-25 15:09:42 +1000

    Working toward pickup completion

---
+++ Assets/Scripts/Interactables/Pickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Pickup", menuName ="Pickup/Empty")]
+public class Pickup : ScriptableObject
+{
+    public string pickupType;
+    public GameObject pickupObject;
+
+    public float healthRestoration;
+    public float jetpackRefuel;
+    public float ammoRestoration;
+}
