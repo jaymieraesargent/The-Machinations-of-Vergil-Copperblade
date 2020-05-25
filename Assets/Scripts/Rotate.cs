10d2038803b0dcda4675ded4f0232c994cd6011e
GlitcherOG <lachlankalf@tafensw.net.au>
2020-05-25 14:48:46 +1000

    Rotate

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
     public float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * speed * Time.deltaTime);
