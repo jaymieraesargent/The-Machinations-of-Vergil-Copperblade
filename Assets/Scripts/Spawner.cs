e1cfea620b1c4546eb1febc62543440aba3c1bc7
GlitcherOG <lachlankalf@tafensw.net.au>
2020-05-25 13:30:54 +1000

    PickupSpawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public bool item;
+    public GameObject prefab;
+    public GameObject Spawnloaction;
+    public float cooldown;
+    float timer;
+
+    private void Start()
+    {
+        item = true;
+        GameObject temp = Instantiate(prefab, Spawnloaction.transform);
+        timer = cooldown;
+    }
+
+    private void Update()
+    {
+        if (!item && timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if (timer <= 0 && !item)
+        {
+            GameObject temp = Instantiate(prefab, Spawnloaction.transform);
+            item = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player" && !item)
+        {
+            item = true;
+            timer = cooldown;
+        }
+    }
+}
